79c366b5ecb4889c2212aa16e8767c5c0958cac0
Dyamez <dyamez@outlook.com>
2023-05-20 11:07:33 -0700

    520-1

--- public/index.js
+++ public/index.js
@@ -4,6 +4,12 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    fetch('https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=2a3ff10c1b3744a8abc3654babd98e69', {
+        header: {
+            //Authorization="apikey 2a3ff10c1b3744a8abc3654babd98e69"
+        }
+    })
+
 }
 
 main()
